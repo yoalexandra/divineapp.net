8455187795c5435fc75e51112ce27fa721c22807
Alexandra <alexandra.beznosova@gmail.com>
2018-11-14 22:41:37 +0300

    urls for email page

--- divineapp/sendemail/forms.py
+++ divineapp/sendemail/forms.py
@@ -1,12 +1,6 @@
 from django import forms
 import re
 
-class GenderField(forms.ChoiceField):
-      def __init__(self, *args, **kwargs):
-            super(GenderField, self).__init__(*args, **kwargs)
-            self.error_messages = {"required":"Please select a gender, it's required"}
-            self.choices = ((None,'Select gender'),('M','Male'),('F','Female'))
-
 class PlaceholderInput(forms.widgets.Input):
       template_name = 'sendemail/placeholder.html'
       input_type = 'text'
@@ -16,23 +10,29 @@ class PlaceholderInput(forms.widgets.Input):
             context['widget']['attrs']['placeholder'] = name.title()
             return context
 
+
 def validate_comment_word_count(value):
       count = len(value.split())
       if count < 30:
             raise forms.ValidationError(('Please provide at least a 30 word message, %(count)s words is not descriptive enough'),
-                                        params={'count': count},)
+                                        params={'count': count},
+            )
+class ContactForm(forms.Form):
+    name = forms.CharField(required=False)
+    email = forms.EmailField(label='Your email')
+    comment = forms.CharField(widget=forms.Textarea)
+
 class ContactForm(forms.Form):
       name = forms.CharField(required=False,widget=PlaceholderInput)
       email = forms.EmailField(label='Your email',widget=PlaceholderInput)
       comment = forms.CharField(widget=forms.Textarea,validators=[validate_comment_word_count],error_messages={"required":"Please, pretty please provide a comment"})
-      gender = GenderField()
-      field_order=['email','name','gender','comment']
+      field_order=['email','name','comment']
       def __init__(self, *args, **kwargs):
             # Get 'initial' argument if any
             initial_arguments = kwargs.get('initial', None)
             updated_initial = initial_arguments
             if initial_arguments:
-                  # If initial arguments, fetch 'user' placeholder variable if any
+                  # We have initial arguments, fetch 'user' placeholder variable if any
                   user = initial_arguments.get('user',None)
                   # Now update the form's initial values if user
                   if user:
@@ -66,8 +66,14 @@ class ContactForm(forms.Form):
             if value.isupper():
                   # Value is all upper case, raise an error
                   raise forms.ValidationError("Please don't use all upper case for your name, use lower case",code='uppercase')
+            # Always return value
             return value
       def clean_email(self):
       	    # Get the field value from cleaned_data dict
             value = self.cleaned_data['email']
+	    # Check if the value end in @hotmail.com
+            if value.endswith('@hotmail.com'):
+                  # Value ends in @hotmail.com, raise an error
+                  raise forms.ValidationError("Please don't use a hotmail email, we simply don't like it",code='hotmail')
+            # Always return value
             return value
--- divineapp/sendemail/views.py
+++ divineapp/sendemail/views.py
@@ -10,14 +10,16 @@ def index(request):
 
 def send_email(request):
     if request.method == 'POST':
-        # POST, generate form with data from the request
+        # POST, generate bound form with data from the request
         form = ContactForm(request.POST)
         # check if it's valid:
         if form.is_valid():
-            # process data, insert into DB, generate email,etc
+            # Insert into DB
+            form.save()
             # redirect to a new URL:
-            return HttpResponseRedirect('/sendemail/send_email/thankyou')
+            return HttpResponseRedirect('/sendemail/contact/thankyou')
     else:
         # GET, generate blank form
-        form = ContactForm(label_suffix=' : ',initial={'user':request.user,'otherstuff':'otherstuff'},use_required_attribute=False,auto_id=False)
+        form = ContactForm(request.POST)
+    #form = ContactForm(label_suffix=' : ',initial={'user':request.user,'otherstuff':'otherstuff'},use_required_attribute=False,auto_id=False)
     return render(request,'sendemail/send_email.html',{'form':form})
